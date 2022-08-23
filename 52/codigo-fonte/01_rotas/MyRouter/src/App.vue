ab908242fcf9dee81688c7a7a08a564357e55603
Willian Sant Anna <w.s.santanna@gmail.com>
2022-08-23 15:21:43 -0300

    Implementação do código fonte da Aula 52

---
+++ 52/codigo-fonte/01_rotas/MyRouter/src/App.vue
@@ -0,0 +1,17 @@
+
+<!-- Lógica: JavaScript -->
+<script setup>
+import Nav from './components/Nav.vue'
+</script>
+
+<!-- View -->
+<!-- View: HTML -->
+<template>
+
+  <h1>Minha Aplicação</h1>
+  <Nav />
+  <RouterView />
+</template>
+<!-- View: CSS -->
+<style scoped>
+</style>
